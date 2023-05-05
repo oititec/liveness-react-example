c2f8b6c5551c46a8f3482c59642800c53be6f17b
André Ferreira Lima <aferlimrockn@msn.com>
2023-05-05 15:52:11 -0300

    feat: env

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
-  mode: 'development',
+  //mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
